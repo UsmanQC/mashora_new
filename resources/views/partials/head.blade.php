dc6863abac25a8a8bb8f25d2bc5f5816c369d0c6
usmanQC <usman@qualitycode.sa>
2026-06-01 12:29:24 +0300

    general changes

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,13 +1,7 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-<title>
-    @if (filled($title ?? null))
-        {{ config('app.name') ? $title.' - '.config('app.name') : $title }}
-    @else
-        {{ config('app.name') }}
-    @endif
-</title>
+<title>{{ config('app.name') }}</title>
 
 <link rel="icon" href="/favicon.ico" sizes="any">
 <link rel="icon" href="/favicon.svg" type="image/svg+xml">
